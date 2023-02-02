bc582bd1fc906dbbaf935e1be7fe72272741fa68
Michael N. Jensen <michael@sys-adm.dk>
2023-02-02 14:48:49 +0100

    just pass parameters directly

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ INPUT_DOCKERFILE=${INPUT_DOCKERFILE:-Dockerfile}
 INPUT_TAG=${INPUT_TAG:-${GITHUB_SHA::8}}
 INPUT_BRANCH=${INPUT_BRANCH:-master}
 IMAGE_PART=""
-if [ -n "$INPUT_BUILD_ARGS" ]; then
-        BUILD_ARGS=`echo -n ${INPUT_BUILD_ARGS:-''} |jq -j '.[] | keys[] as $k | values[] as $v |  "--build-arg \($k)=\"\($v)\" "'`
-fi
+# if [ -n "$INPUT_BUILD_ARGS" ]; then
+#         BUILD_ARGS=`echo -n ${INPUT_BUILD_ARGS:-''} |jq -j '.[] | keys[] as $k | values[] as $v |  "--build-arg \($k)=\"\($v)\" "'`
+# fi
 
 if [ "$INPUT_IMAGE" != "" ]; then
         IMAGE_PART="/${INPUT_IMAGE}"
@@ -23,4 +23,4 @@ echo "Logging into azure.."
 az login --service-principal -u ${INPUT_SERVICE_PRINCIPAL} -p ${INPUT_SERVICE_PRINCIPAL_PASSWORD} --tenant ${INPUT_TENANT}
 
 echo "Sending build job to ACR.."
-az acr build -r ${INPUT_REGISTRY} ${BUILD_ARGS} -f ${INPUT_DOCKERFILE} -t ${INPUT_REPOSITORY}${IMAGE_PART}:${INPUT_TAG} https://${GIT_ACCESS_TOKEN_FLAG}github.com/${GITHUB_REPOSITORY}.git#${INPUT_BRANCH}:${INPUT_FOLDER}
+az acr build -r ${INPUT_REGISTRY} ${INPUT_BUILD_ARGS} -f ${INPUT_DOCKERFILE} -t ${INPUT_REPOSITORY}${IMAGE_PART}:${INPUT_TAG} https://${GIT_ACCESS_TOKEN_FLAG}github.com/${GITHUB_REPOSITORY}.git#${INPUT_BRANCH}:${INPUT_FOLDER}
